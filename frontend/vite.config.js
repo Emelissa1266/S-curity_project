3a52aa1490eb39ba40db0fe62a75439eb1a58ae1
MELISSA JOUDA <150617628+Emelissa1266@users.noreply.github.com>
2026-02-26 16:25:31 +0100

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
       port: 5173,
       proxy: {
         '/api': {
-          target: env.VITE_API_URL || 'https://s-curity-project.onrender.com',
+          target: 'https://s-curity-project.onrender.com',
           changeOrigin: true
         }
       }
